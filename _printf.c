7fa4a9f708619b9de176977867e8966eb2800c33
erinscarter <erin.s.carter@gmail.com>
2023-03-13 18:22:28 -0500

    function update

--- _printf.c
+++ _printf.c
@@ -1,29 +1,58 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
-* _printf - writes the character c to stdout
-* @format: check for data type
-* Return: Always 0
+* _printf - recreation of printf to find certain specifiers
+* Description: Funtion that prints string format.
+*
+* @format: arguments function
+* Return: an int len of string to '\0'
 */
-
 int _printf(const char *format, ...)
+
 {
-	int printed_chars;
-	convert_t f_list[] = {
-	{"c", pr_char},
-	{"s", pr_string},
-	{"%", pr_percent},
-	{"d", pr_decimal},
-	{"i", pr_int},
-													};
-	va_list arg_list;
+      	int i = 0, bf_count = 0;
+	char *buffer = NULL;
+	void (*category_functions)(char *, va_list, int *);
+	va_list args;
 
-	if (format == NULL)
-		return (-1);
+	va_start(args, format), buffer = malloc(sizeof(char) * 2048);
+		if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+				return (-1);
+		if (!format[i])
+				return (0);
+		for (; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '\0')
+					continue;
 
-	va_start(arg_list, format);
-	/*Calling parser function*/
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+				category_functions = get_category_functions(format[i + 1]);
+				if (category_functions == NULL)
+				{
+					buffer[bf_count] = format[i];
+
+				if (format[i + 1] != '%')
+				{
+					buffer[bf_count + 1] = format[i + 1];
+					bf_count += 2;
+				}
+				else
+					bf_count++;
+				}
+				else
+					category_functions(buffer, args, &bf count);
+					i++;
+					continue;
+				}
+				buffer[bf_count] = format[i];
+				bf_count++;
+				}
+				va_end(args), write(1, buffer, bf_count, free(buffer);
+				return (bf_count);
 }
+
+
